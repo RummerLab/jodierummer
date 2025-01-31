f93b7eb39681b532ad5c888eb980f9a985764b45
RummerLab <webmaster@rummerlab.com>
2025-01-31 22:25:15 +1000

    sitemap edited

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,11 +3,10 @@ module.exports = {
     siteUrl: 'https://jodierummer.com',
     generateRobotsTxt: true,
     sitemapSize: 7000,
-    changefreq: 'weekly',
+    changefreq: 'monthly',
     priority: 0.7,
     exclude: ['/private*'],
     robotsTxtOptions: {
-        additionalSitemaps: ['https://jodierummer.com/server-sitemap.xml'],
         policies: [
             {
                 userAgent: '*',
