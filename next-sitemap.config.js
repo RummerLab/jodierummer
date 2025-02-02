c017c7b320c287443f4573f8d9b03f3e361056c0
RummerLab <webmaster@rummerlab.com>
2025-02-02 12:50:14 +1000

    edit sitemap to state homepage updates daily.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,6 +6,16 @@ module.exports = {
     changefreq: 'monthly',
     priority: 0.5,
     exclude: ['/private*'],
+    additionalPaths: async (config) => {
+        return [
+            {
+                loc: '/',
+                changefreq: 'daily',
+                priority: 1,
+                lastmod: new Date().toISOString(),
+            },
+        ]
+    },
     robotsTxtOptions: {
         policies: [
             {
