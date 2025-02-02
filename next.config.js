db333605fbc873af79a1e603ac304f4df3b5baf8
RummerLab <webmaster@rummerlab.com>
2025-02-02 15:11:14 +1000

    .

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
                 hostname: 'rummerlab.com',
                 pathname: '/images/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'physiology.org',
+                pathname: '/images/**',
+            },
         ],
     },
     redirects: async () => [
